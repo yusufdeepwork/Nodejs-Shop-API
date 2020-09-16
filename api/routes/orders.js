10514487de580027a33e793ea0ef91ca82d8e82d
yusufdeepwork <yusuf.deepwork@gmail.com>
2020-09-16 13:17:47 +0300

    added a logger (morgan), nodemon and error handling

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
+
+// Handle incoming GET requests to /orders
 router.get('/',(req,res,next) => {
     res.status(200).json({
         message : 'Orders were fetched'
