b43405cff4713baa72419b371754e756a16aeb73
yusufdeepwork <yusuf.deepwork@gmail.com>
2020-09-17 11:34:58 +0300

    added mongoose and product database functions

--- app.js
+++ app.js
@@ -2,9 +2,12 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
+
+mongoose.connect('mongodb+srv://node-rest-api:'+ process.env.MONGO_ATLAS_PW +'@node-rest-api.oskjp.mongodb.net/<dbname>?retryWrites=true&w=majority');
 
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({extended : false}));
