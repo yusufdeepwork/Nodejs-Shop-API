bd36b754c6bc1c9f57ff905d3848241d70247df6
yusufdeepwork <yusuf.deepwork@gmail.com>
2020-09-20 12:59:56 +0300

    added logic to populate orders   with (selected) product data

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -9,9 +9,9 @@ const Product = require('../models/product');
 
 // Handle incoming GET requests to /orders
 router.get('/',(req,res,next) => {
-     
     Order.find()
     .select("product quantity _id")
+    .populate('product','name')
     .exec()
     .then(docs =>{
         res.status(200).json({
@@ -80,6 +80,7 @@ router.post('/',(req,res,next) => {
 
 router.get('/:orderId',(req,res,next)=>{
     Order.findById(req.params.orderId)
+    .populate('product')
     .exec()
     .then(order => {
         if(!order){
