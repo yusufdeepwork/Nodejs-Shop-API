8ea5d6c6d7db9b85c947a48953d94d7ab8e3f8dc
yusufdeepwork <yusuf.deepwork@gmail.com>
2020-09-21 13:52:08 +0300

    added auth-signup

--- app.js
+++ app.js
@@ -6,9 +6,10 @@ const mongoose = require('mongoose');
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
+const userRoutes = require('./api/routes/user');
 
 mongoose.connect('mongodb+srv://node-rest-api:'+ process.env.MONGO_ATLAS_PW +'@node-rest-api.oskjp.mongodb.net/<dbname>?retryWrites=true&w=majority',
-{ useNewUrlParser: true,useUnifiedTopology: true });
+{ useNewUrlParser: true,useUnifiedTopology: true,useCreateIndex: true });
 
 app.use(morgan('dev'));
 app.use('/uploads', express.static('uploads'));
@@ -31,6 +32,8 @@ app.use((req,res,next) => {
 //Routers which should handle requests
 app.use('/products',productRoutes);
 app.use('/orders',orderRoutes);
+app.use('/user',userRoutes);
+
 
 app.use((req,res,next)=>{
     const error = new Error('Not Found');
